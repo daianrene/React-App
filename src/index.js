8c7d2088d5194e6964326979b1ec4c9a466aad30
Day <daianrene99@gmail.com>
2021-08-12 17:34:23 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const user = {
+    nombre: 'Day',
+    apellido: 'Rene',
+    avatar: "https://static.thenounproject.com/png/157453-200.png"
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const getName2 = (user)=>{
+    return `${user.nombre} ${user.apellido} `
+}
+
+const getName3= (user)=>{
+    if(user){
+        return <h1>Holiwis {getName2(user)}</h1>
+    }else{
+        return <h1>Naonao</h1>
+    }
+}
+
+
+const element = (
+    <div>
+        {getName3(user)}
+        <img src= {user.avatar}/>
+    </div>
+)
+
+const container = document.getElementById('root')
+
+ReactDom.render(element,container)
